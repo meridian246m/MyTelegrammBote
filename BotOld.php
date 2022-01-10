fda5021405e518f409610a23fb373dc73a6bbc46
meridian246 <meridian246m@gmail.com>
2022-01-10 23:16:55 +0200

    first commit

--- BotOld.php
+++ BotOld.php
@@ -69,7 +69,7 @@
 //https://api.telegram.org/bot+ТокенБота+/setwebhook?url=+СсылкаНаБота
 //Связать
 //https://api.telegram.org/bot123asd123asd123asd/setwebhook?url=https://test123123.ru/telegram_bot.php
-//https://api.telegram.org/bot1238564789:AAF1kydnaZ_ZWXlBrCXVyKC5RVeOLynCMvg/setwebhook?url=https://telebot.tesovii.space/Telebot.php
+//https://api.telegram.org/bot1238564789:AAF1kydnaZ_ZWXlBrCXVyKC5RVeOLynCMvg/setwebhook?url=https://telebot.tesovii.space/telebot.php
 
 //Отвязать
 //https://api.telegram.org/bot+ТокенБота+/deleteWebhook
@@ -79,6 +79,134 @@
 //$data = json_decode(file_get_contents('php://input'), TRUE);
 //пишем в файл лог сообщений
 //file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+
+$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+
+define('TOKEN', '1238564789:AAF1kydnaZ_ZWXlBrCXVyKC5RVeOLynCMvg');
+
+$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
+
+
+switch ($message) {
+    case 'да':
+        $method = 'sendMessage';
+		$send_data = [
+                        'text' => 'Что вы хотите заказать?',
+                        'reply_markup'  => [
+                                                'resize_keyboard' => true,
+                                                'keyboard' =>   [
+                                                                    [
+                                                                        ['text' => 'Яблоки'],
+                                                                        ['text' => 'Груши'],
+                                                                    ],
+                                                                    [
+                                                                        ['text' => 'Лук'],
+                                                                        ['text' => 'Чеснок'],
+                                                                    ]
+                                                                ]
+                                            ]
+        			];
+    break;
+	case 'нет':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'Приходите еще'];
+    break;
+	case 'яблоки':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'груши':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'лук':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	case 'чеснок':
+        $method = 'sendMessage';
+		$send_data = ['text' => 'заказ принят!'];
+    break;
+	default:
+		$method = 'sendMessage';
+		$send_data = [
+			'text' => 'Вы хотите сделать заказ?',
+			'reply_markup'  => [
+				'resize_keyboard' => true,
+				'keyboard' => [
+						[
+							['text' => 'Да'],
+							['text' => 'Нет'],
+						]
+					]
+				]
+			];
+}
+
+$send_data['chat_id'] = $data['chat'] ['id'];
+
+$res = sendTelegram($method, $send_data);
+
+
+
+
+function sendTelegram($method, $data, $headers = [])
+{
+	$curl = curl_init();
+	curl_setopt_array($curl, [
+		CURLOPT_POST => 1,
+		CURLOPT_HEADER => 0,
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+		CURLOPT_POSTFIELDS => json_encode($data),
+		CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"))
+	]);
+	$result = curl_exec($curl);
+	curl_close($curl);
+	return (json_decode($result, 1) ? json_decode($result, 1) : $result);
+}
+
+
+
+
+
+<?php
+//include "MyBot.php";
+
+//$data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
+//$message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
+
+//$WorkBot = New MyBot;
+//$send_data = $WorkBot->SwitchCaseBot($message);
+//$send_data = 'lf';
+//$send_data['chat_id'] = $data['chat'] ['id'];
+//$res = $WorkBot->SendMessage('sendMessage', $send_data);
+
+/*
+ if (array_key_exists('message', $data))// проверяем если пришло сообщение
+{
+    if ($data['message']['text'] == "/start")    //если пришла команда /start
+    {
+        $this->sendMessage($chat_id, "Приветствую! Загрузите картинку.");
+    } 
+    elseif 
+    
+    (array_key_exists('photo', $data['message'])) 
+    {
+        // если пришла картинка то сохраняем ее у себя
+        $text = $this->getPhoto($data['message']['photo'])
+            ? "Спасибо! Можете еще загрузить мне понравилось их сохранять."
+            : "Что-то пошло не так, попробуйте еще раз";
+        // отправляем сообщение о результате   
+        $this->sendMessage($chat_id, $text);
+
+    } else 
+    {
+        // если пришло что-то другое
+        $this->sendMessage($chat_id, "Не понимаю команду! Просто загрузите картинку.");
+    }
+}
+*/
 
 $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
 
--- MyBot.php
+++ MyBot.php
@@ -1,82 +1,64 @@
 <?php
     class MyBot 
-    {
-            private     $botName =          '@md246bot';
-            protected   $botToken =       '1238564789:AAF1kydnaZ_ZWXlBrCXVyKC5RVeOLynCMvg';
-            //method sendMessage
-        public function  GetBotName(){return $this->botName;}
-        public function  GetBotToken(){return $this->botToken;}
-        public function  SetBotName($bName){$this->botName = $bName;}
-        public function  SetBotToken($bToken){$this->botName = $bToken;}
-    
-    private function getPhoto($data)// общая функция загрузки картинки
-    {
-        $file_id = $data[count($data) - 1]['file_id'];     	// берем последнюю картинку в массиве
-        $file_path = $this->getPhotoPath($file_id);         // получаем file_path
-        return $this->copyPhoto($file_path);                // возвращаем результат загрузки фото
-    }
+{
+            public           $botToken = '1238564789:AAF1kydnaZ_ZWXlBrCXVyKC5RVeOLynCMvg';
+            public function  GetBotToken(){return $this->botToken;}
+            public function  SetBotToken($bToken){$this->botToken = $bToken;}
+            public function  sendMessage($method, $data, $token, $headers = [])
+            {
+                $curl = curl_init();
+                curl_setopt_array($curl, [
+                    CURLOPT_POST => 1,
+                    CURLOPT_HEADER => 0,
+                    CURLOPT_RETURNTRANSFER => 1,
+                    CURLOPT_URL => 'https://api.telegram.org/bot' . $token . '/' . $method,
+                    CURLOPT_POSTFIELDS => json_encode($data),
+                    CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"))
+                ]);
+                $result = curl_exec($curl);
+                curl_close($curl);
+                return (json_decode($result, 1) ? json_decode($result, 1) : $result);
+            }
+}
 
-    private function getPhotoPath($file_id)                 // функция получения метонахождения файла
+    class DataBase
     {
-        $array = json_decode($this->requestToTelegram(['file_id' => $file_id], "getFile"), TRUE);     	// получаем объект File
-        return  $array['result']['file_path'];              // возвращаем file_path
-    }
+            private $servername = "aa389941.mysql.tools";
+            private $database = "aa389941_botbase";
+            private $username = "aa389941_meridian246";
+            private $password = "96stYP8BMf7h";
 
-    private function copyPhoto($file_path)              // копируем фото к себе
-    {
-        $file_from_tgrm = "https://api.telegram.org/file/bot".$this->botToken."/".$file_path;    	// ссылка на файл в телеграме
-        $ext =  end(explode(".", $file_path));                                                      // достаем расширение файла
-        $name_our_new_file = time().".".$ext;                                                       // назначаем свое имя здесь время_в_секундах.расширение_файла
-        return copy($file_from_tgrm, "img/".$name_our_new_file);
-    }
-
-        public function GetUpdates($data)
+        public function DataBaseExecute($sql)
         {
-            $data = $data['callback_query'] ? $data['callback_query'] : $data['message'];
-            $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8');
-            return $message;
+            $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+            if (!$conn) {die("Connection failed: " . mysqli_connect_error());} else {echo "Connected successfully";}
+            $result = mysqli_query($conn, $sql); 
+            mysqli_close($conn);
+            return $result;
         }
-
-        public function GetImage($data)
+        public function TestChatId($chat_id)
         {
-            $text = $this->getPhoto($data['message']['photo'])
-                ? "Спасибо! Можете еще загрузить мне понравилось их сохранять."
-                : "Что-то пошло не так, попробуйте еще раз";
+            $sql="SELECT 1 FROM data_user WHERE chat_id=".$chat_id;
+            $result = mysqli_fetch_assoc($this->DataBaseExecute($sql));
+            if(count($result)>0){return true;}else{return false;}
         }
+        public function InsertNewUser($chat_id)
+        {
+            $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+            if (!$conn) {die("Connection failed: " . mysqli_connect_error());} else {echo "Connected successfully";}
+            $result = mysqli_query($conn, $sql); 
+            mysqli_close($conn);
+            return $result;
+        }
+        public function CreateUser($chat_id)
+        {
+            $sql = "INSERT INTO data_user (chat_id,Name,City,Busines,AboutSelf,WhoSearch,Img) VALUES ('$chat_id','na','na',0,'na',0,na)";
+            $result = mysqli_fetch_assoc($this->DataBaseExecute($sql));
+            return $result['chat_id'];
+        }
+    //    public function SetPhotoStore(){}
 
-            /** Отправляем запрос в Телеграмм
-         * @param $data
-         * @param string $type
-         * @return mixed
-         */
-    private function requestToTelegram($data, $type)
-    {
-        $result = null;
-        if (is_array($data)) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $this->apiUrl . $this->botToken . '/' . $type);
-            curl_setopt($ch, CURLOPT_POST, count($data));
-            curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-            $result = curl_exec($ch);
-            curl_close($ch);
-        }
-        return $result;
-    }
 
-        public function SendMessage($method, $data, $headers = []){
-            $TOKEN = $this->GetBotToken();
-            $curl = curl_init();
-            curl_setopt_array($curl, [
-                CURLOPT_POST => 1,
-                CURLOPT_HEADER => 0,
-                CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => 'https://api.telegram.org/bot' . $TOKEN . '/' . $method,
-                CURLOPT_POSTFIELDS => json_encode($data),
-                CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"))
-            ]);
-            $result = curl_exec($curl);
-            curl_close($curl);
-            return (json_decode($result, 1) ? json_decode($result, 1) : $result);
-        }
     }
+?>
+
